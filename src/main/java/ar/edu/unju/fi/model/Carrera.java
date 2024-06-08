a1622d558681de32b6ff7a720b90bc7c9b7113a9
alan <alan@DESKTOP-HPCK8IS>
2024-06-08 07:03:06 -0300

    comienzo tp4 parte2

--- src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -5,4 +5,50 @@ public class Carrera {
 	private String nombre;
 	private Byte cantAnios;
 	private String estado;
+	
+	public Carrera() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Carrera(String codigo, String nombre, Byte cantAnios, String estado) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.cantAnios = cantAnios;
+		this.estado = estado;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Byte getCantAnios() {
+		return cantAnios;
+	}
+
+	public void setCantAnios(Byte cantAnios) {
+		this.cantAnios = cantAnios;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
 }
